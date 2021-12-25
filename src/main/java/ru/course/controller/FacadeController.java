15a6976c7e29e99a79f3a9bd914c00683edce9a0
Pavel Chupikov <toppmote@gmail.com>
2021-12-26 05:06:39 +0700

    Добавил генерацию пользователей

--- src/main/java/ru/course/controller/FacadeController.java
+++ src/main/java/ru/course/controller/FacadeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import ru.course.systemClasses.SystemManager;
 
 /**
  * Контроллер для запросов от клиента
@@ -13,16 +14,30 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class FacadeController {
 
+    SystemManager systemManager;
+
     /**
      * Метод обработки запроса на загрузку начальной страницы
      *
      * @return Model
      */
     @GetMapping("/")
-    public ModelAndView loadIndexPage(Model model) {
+    public ModelAndView loadIndexPage() {
         ModelAndView index = new ModelAndView("index");
         log.info("GET\tLoaded starting page");
         return index;
+    }
+
+    /**
+     * Метод обработки запроса на нажатие кнопки "Начать"
+     *
+     * @return Model
+     */
+    public ModelAndView startSystem() {
+        systemManager = new SystemManager();
+        systemManager.initUsers();
+
+        return new ModelAndView("index");
     }
 
 }
